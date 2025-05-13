c243bdcaff45175af130acf9b43fd21c52d77352
Akshaj6 <akshaj.samaria@gmail.com>
2025-05-13 17:30:33 +0530

    62

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+tuplex = ('nested', 1, 3.5, False)
+print(tuplex)
+tuplex = (4, 6, 8, 10)
+print(tuplex)
+tuplex = tuplex + (4, 7, 1, 23,)
+print(tuplex)
+tuple1 = (34, 67 , 89)
+print(tuple1.count(67))
+tuple = (1, 2, 3, 4, 5, 6)
+slice = tuplex[2 : 5 : 2]
+print(slice)
+_slice = tuplex[:6]
+print(_slice)
